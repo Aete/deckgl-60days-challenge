c433d60a100fed88a0bd9586d9184844f45fe2c7
Seunggyun Han <hsg8234@naver.com>
2021-02-25 20:25:05 +0900

    Feature: Day 8 - Heatmap Layer with Citibike dataset

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import DayFour from './DayFour/DayFour';
 import DayFive from './DayFive/DayFive';
 import DaySix from './DaySix/DaySix';
 import DaySeven from './DaySeven/DaySeven';
+import DayEight from './DayEight/DayEight';
 
 function App() {
   return (
     <div className="App">
-      <DaySeven />
+      <DayEight />
     </div>
   );
 }
